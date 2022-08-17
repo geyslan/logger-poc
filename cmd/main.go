f7335d5003d2980739fec281724db66afa5a1fba
Geyslan Gregório <geyslan@gmail.com>
2022-08-17 15:23:44 -0300

    use different writers

--- cmd/main.go
+++ cmd/main.go
@@ -31,37 +31,39 @@ func (aw *AnyWriter) Write(message []byte) (n int, err error) {
 }
 
 func generateModLogs(log *logger.Logger) {
+	log.WarnfModule(EBPFModule, "%d %s", 1, "test", errors.New("new warn").Error())
 	log.WarnfModule(EBPFModule, "%d %s", 2, "test", errors.New("new warn").Error())
-	log.WarnfModule(EBPFModule, "%d %s", 2, "test", errors.New("new warn").Error())
-	log.WarnfModule(EBPFModule, "%d %s", 2, "test", errors.New("new warn").Error())
+	log.WarnfModule(EBPFModule, "%d %s", 3, "test", errors.New("new warn").Error())
 }
 
 func generatePkgLogs(log *logger.Logger) {
-	log.WarnfPkg("%d %s", 1, "test", errors.New("new warn").Error())
-	log.WarnfPkg("%d %s", 2, "test", errors.New("new warn").Error())
-	log.WarnfPkg("%d %s", 3, "test", errors.New("new error").Error())
+	log.WarnfPkg("%d %s", 4, "test", errors.New("new warn").Error())
+	log.WarnfPkg("%d %s", 5, "test", errors.New("new warn").Error())
+	log.WarnfPkg("%d %s", 6, "test", errors.New("new error").Error())
 }
 
 func main() {
-	anyWriter := InitAnyWriter()
+	anyWriterMod := InitAnyWriter()
 
 	logMod := logger.Init()
-	logMod.ConfigureMod(anyWriter, EBPFModule|EnrichModule)
+	logMod.ConfigureMod(anyWriterMod, EBPFModule|EnrichModule)
 
 	go generateModLogs(logMod)
 	fmt.Println(strings.Repeat("=", 5), "by bitwised modules")
 	for i := 0; i < 3; i++ {
-		log := <-anyWriter.LogChannel
+		log := <-anyWriterMod.LogChannel
 		fmt.Printf("%v", log)
 	}
 
+	anyWriterPkg := InitAnyWriter()
+
 	logPkg := logger.Init()
-	logPkg.ConfigurePkg(anyWriter, "main")
+	logPkg.ConfigurePkg(anyWriterPkg, "main")
 
 	go generatePkgLogs(logPkg)
 	fmt.Println(strings.Repeat("=", 5), "by package names")
 	for i := 0; i < 3; i++ {
-		log := <-anyWriter.LogChannel
+		log := <-anyWriterPkg.LogChannel
 		fmt.Printf("%v", log)
 	}
 }
